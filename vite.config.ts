b0a8e31116b4e7686c60dbfdf7d1e18468d3aec9
eareddhel <eareddhel@gmail.com>
2026-06-06 22:54:39 -0400

    Agregar configuración de construcción en vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { defineConfig } from "vite"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    emptyOutDir: true,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
